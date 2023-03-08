3620d5bad9e71bbc0fa8ab9e659a50a887233b66
vika-sk <viktoriya-skryabina@mail.ru>
2023-03-08 15:48:37 +0300

    add zadacha001

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// задача 1
+// напишите прогр кот выводит случ число из отрезка
+//[10 , 99] и показ наиб цифру цифру числа
+
+Console.Clear();
+int n =  new Random().Next(10, 100); // [10, 99]
+int n1 = n / 10; // 98 / 10 = 9
+int n2 = n % 10; // 98 % 10 = 8
+Console.WriteLine($"Число: {n}");
+
+if (n1 > n2)
+    Console.WriteLine(n1);
+else if (n2 > n1)
+    Console.WriteLine(n2);
+else
+Console.WriteLine("=");
